cad277e4ef474478da0d1e10f2d692740a001982
Jacob Marthaller <https://github.com/jmarthaller>
2021-02-01 17:40:06 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function focusEvent(event) {
+        console.log('Good!');
+    }
+
+    function blurEvent(event) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (<button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>)
+
+}
+
+
+
+export default EyesOnMe;
